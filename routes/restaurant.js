0e1cadd5ecbacf90a45fdbc6fa309d6761f0cbfd
snehalkale41995 <kalesnehal1007@gmail.com>
2020-06-02 15:34:35 +0530

    added order, cart Api

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -24,4 +24,23 @@ router.get("/menuItems", async (req, res) => {
   res.send(result.recordset);
 });
 
-module.exports = router;
+
+router.get("/shoppingCart/:userId", async (req, res) => {
+    const pool = await poolPromise;
+    const result = await pool.request()
+      .query(`SELECT shop.Id, shop.ApplicationUserId, Menu.Name, Menu.Image , Menu.Price, shop.Count
+                FROM [dbo].[MenuItem] Menu INNER JOIN [dbo].[ShoppingCart] Shop ON Shop.MenuItemId = Menu.Id where shop.ApplicationUserId = ${req.params.userId}`);
+     res.send(result.recordset);
+  });
+
+
+  router.get("/orders/:userId", async (req, res) => {
+    const pool = await poolPromise;
+    const result = await pool.request()
+      .query(`SELECT OrderHeader.* , OrderDetails.MenuItemId,   OrderDetails.Count,  OrderDetails.Name, OrderDetails.Price
+              FROM [dbo].[OrderHeader] OrderHeader INNER JOIN [dbo].[OrderDetails] [OrderDetails] ON OrderHeader.Id = OrderDetails.OrderId 
+              WHERE OrderHeader.UserId = ${req.params.userId} `);
+     res.send(result.recordset);
+  });
+
+   module.exports = router;
