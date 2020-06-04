5b97fab5737138976f5cb214bf6105a9cae520f7
snehalkale41995 <kalesnehal1007@gmail.com>
2020-06-04 13:15:24 +0530

    login and register api

---
+++ database/appConfig.js
@@ -0,0 +1,9 @@
+var appConfig = 
+   {
+     azureUrl: 'spice20200528131853.azurewebsites.net'
+   }
+
+
+   module.exports = {
+    appConfig
+   }
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,84 @@
 const express = require("express");
 const router = express.Router();
 const { poolPromise } = require("../database/db");
-const winston = require("winston");
-const Joi = require("joi");const express = require("express");
-const router = express.Router();
-const { poolPromise } = require("../database/db");
-const winston = require("winston");
+var axios = require("axios");
 const Joi = require("joi");
+var http = require("https");
+var qs = require("querystring");
+const {appConfig} = require("../database/appConfig")
 
 
+router.post("/login", async (request, response) => {
+    let { Email, Password } = request.body;
+    var data;
+    var options = {
+        "method": "POST",
+        "hostname": `${appConfig.azureUrl}`,
+        "port": null,
+        "path": "/Customer/Home/Login",
+        "headers": {
+          "content-type": "application/x-www-form-urlencoded",
+          "cache-control": "no-cache",
+          "postman-token": "42021288-e54a-9587-3076-c7709d3980fa",
+          "accept": "application/json, text/plain, */*"
+        }
+      };
+      var req = http.request(options, function (res) {
+        var chunks = [];
+       
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+      
+        res.on("end", function () {
+          data = Buffer.concat(chunks);
+          response.status(201).send(JSON.parse(data));
+        });
+      });
+      
+      req.write(qs.stringify({ Email: Email , Password : Password }));
+      req.end();
+  });
+
+  router.post("/register", async (request, response) => {
+      console.log("req.body", request.body)
+      let {Name,  Email, PhoneNumber, StreetAddress, State,  PostalCode, Password, City} = request.body;
+      var data;
+      var options = {
+        "method": "POST",
+        "hostname": `${appConfig.azureUrl}`,
+        "port": null,
+        "path": "/Customer/Home/Register",
+        "headers": {
+          "content-type": "application/x-www-form-urlencoded",
+          "cache-control": "no-cache",
+          "postman-token": "d292b3dc-e74b-dce3-8dbd-d15b0e21067c"
+        }
+      };
+      var req = http.request(options, function (res) {
+        var chunks = [];
+      
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+      
+        res.on("end", function () {
+          data = Buffer.concat(chunks);
+          response.status(201).send(JSON.parse(data));
+        });
+      });
+      
+      req.write(qs.stringify({ 
+        Name: Name,
+        Email: Email,
+        PhoneNumber: PhoneNumber,
+        StreetAddress: StreetAddress,
+        State: State,
+        PostalCode: PostalCode,
+        Password: Password,
+        City: City }));
+        req.end();
+
+  })
+
+module.exports = router;
